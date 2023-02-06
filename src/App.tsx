c3df3231940baf605b348dbdc5dfb5333afa640e
ddx <aditya.saputra@brilife.co.id>
2023-02-06 11:22:57 +0700

    [ADD] form login with formik and yup validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import Login from './components/login/login'
-import './App.scss'
+import BGIMG from './assets/images/svg/bg-login-digiform.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-img">
-      <Login />
-    </div>
+    <div className='h-screen bg-img-login bg-no-repeat bg-cover'>
+      < Login />
+    </div >
   )
 }
 
